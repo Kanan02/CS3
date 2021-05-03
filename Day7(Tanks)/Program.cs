fa93d2198a88ae5c640d5d92e4c88f3293e34bb3
Gurb_bx51@itstep.edu.az <Gurb_bx51@itstep.edu.az>
2021-05-03 20:56:52 +0400

    Add project files.

---
+++ Day7(Tanks)/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib.WordOfTanks;
+namespace Day7_Tanks_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "WordOfTanks";
+            
+            Tank[] tanks1 = new Tank[5]
+            {
+                new Tank("T-34"),
+                new Tank("T-34"),
+                new Tank("T-34"),
+                new Tank("T-34"),
+                new Tank("T-34")
+            };
+            Tank[] tanks2 = new Tank[5]
+             {
+                new Tank("Pantera"),
+                new Tank("Pantera"),
+                new Tank("Pantera"),
+                new Tank("Pantera"),
+                new Tank("Pantera")
+             };
+            //foreach (var item in tanks1)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine();
+            //foreach (var item in tanks2)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            for (int i = 0; i < tanks1.Length; i++)
+            {
+                Console.WriteLine(tanks1[i]*tanks2[i]);
+            }
+        }
+    }
+}
---
+++ Day7(Tanks)/WordOfTanks/Tank.cs
@@ -0,0 +1,65 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyClassLib.WordOfTanks
+{
+    
+    class Tank
+    {
+        private static readonly Random random = new Random();
+        private static readonly object syncLock = new object();
+        private static int RandomNumber(int min, int max)
+        {
+            lock (syncLock)
+            { 
+                return random.Next(min, max);
+            }
+        }
+        public string Name { get; set; }
+       
+        public int AmmunitionLevel { get; set; }
+        public int Armor { get; set; }
+        public int Maneuverability { get; set; }
+        
+        
+        public Tank(string name)
+        {
+            AmmunitionLevel = RandomNumber(0, 100);
+            Armor = RandomNumber(0, 100);
+            Maneuverability = RandomNumber(0, 100);
+
+            Name = name;
+            
+        }
+
+        public static string operator *(Tank t1,Tank t2)
+        {
+            
+            string str = "\t\tBattle!\n";
+
+            str += $"   {t1.Name}                       {t2.Name}\n";
+            str += $"AmmunitionLevel:{t1.AmmunitionLevel}        AmmunitonLevel:{t2.AmmunitionLevel}\n";
+            str += $"Armor:{t1.Armor}                  Armor:{t2.Armor}\n";
+            str += $"Maneuverability:{t1.Maneuverability}        Maneuverability:{t2.Maneuverability}\n";
+           
+            if (t1.AmmunitionLevel > t2.AmmunitionLevel && t1.Armor > t2.Armor) str += $"\t\t{t1.Name} wins.\n";
+            else if (t1.AmmunitionLevel > t2.AmmunitionLevel && t1.Maneuverability > t2.Maneuverability) str += $"\t\t{t1.Name} wins.\n";
+            else if (t1.Maneuverability > t2.Maneuverability && t1.Armor > t2.Armor) str += $"\t\t{t1.Name} wins.\n";
+            else if (t1.AmmunitionLevel < t2.AmmunitionLevel && t1.Armor < t2.Armor) str += $"\t\t{t2.Name} wins.\n";
+            else if (t1.AmmunitionLevel < t2.AmmunitionLevel && t1.Maneuverability < t2.Maneuverability) str += $"\t\t{t2.Name} wins.\n";
+            else if (t1.Maneuverability < t2.Maneuverability && t1.Armor < t2.Armor) str += $"\t\t{t2.Name} wins.\n";
+            else str+= "\t\tDraw!\n";
+            str += "------------------------------------\n";
+            return str;
+
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}\nAmmunition Level: {AmmunitionLevel}\nArmor: {Armor}\nManeuverability: {Maneuverability}\n"; ;
+        }
+
+
+    }
+}
